8c2b0cca9eb35d5d13034f93cfa88efc816362f5
Shreyaskar <shreyash0790@gmail.com>
2023-06-08 21:11:10 +0530

    test comit

--- main.js
+++ main.js
@@ -84,14 +84,14 @@ function showOutput(res) {
 }
 
 // Event listeners
-document.getElementById('get').addEventListener('click', getTodos);
-document.getElementById('post').addEventListener('click', addTodo);
-document.getElementById('update').addEventListener('click', updateTodo);
-document.getElementById('delete').addEventListener('click', removeTodo);
-document.getElementById('sim').addEventListener('click', getData);
-document.getElementById('headers').addEventListener('click', customHeaders);
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
-document.getElementById('error').addEventListener('click', errorHandling);
-document.getElementById('cancel').addEventListener('click', cancelToken);
+// document.getElementById('get').addEventListener('click', getTodos);
+// document.getElementById('post').addEventListener('click', addTodo);
+// document.getElementById('update').addEventListener('click', updateTodo);
+// document.getElementById('delete').addEventListener('click', removeTodo);
+// document.getElementById('sim').addEventListener('click', getData);
+// document.getElementById('headers').addEventListener('click', customHeaders);
+// document
+//   .getElementById('transform')
+//   .addEventListener('click', transformResponse);
+// document.getElementById('error').addEventListener('click', errorHandling);
+// document.getElementById('cancel').addEventListener('click', cancelToken);
